6d3ad9732478ec3ebff16a6b38089f2daf478fc7
ggwin-go <ggwin-go@yandex.ru>
2023-07-01 11:30:39 +0300

    detection chorus line changed; refactored

--- src/containers/Song/Song.tsx
+++ src/containers/Song/Song.tsx
@@ -3,13 +3,13 @@ import cn from 'classnames';
 
 import { getChordsFromString } from '../../utils/stringHelper';
 import { scrollToTop } from '../../utils/helper';
+import { useWindowResize } from '../../hooks/windowEventHooks';
 import { ISong, ISongRow, SongsByAuthorsData } from '../../constants/SongsData';
 import { useAppNavigation } from '../../components/Navigation';
 import ChordsRowPopUp from '../../components/ChordsRowPopUp';
+import { generateLyrics, isChorusLine } from './helper';
 
 import styles from './Song.module.scss';
-import { getTextWidth } from '../../utils/canvasHelper';
-import { useWindowResize } from '../../hooks/windowEventHooks';
 
 type SongProps = {
   author: string;
@@ -28,9 +28,13 @@ const Song: FC<SongProps> = ({ author, song: { name, lyrics, speed: defaultSpeed
   const [adaptiveLyrics, setAdaptiveLyrics] = useState<ISongRow[]>([]);
   const [fontSize, setFontSize] = useState(18);
 
+  function generateWrappedLyrics() {
+    setAdaptiveLyrics(generateLyrics(lyrics, fontSize));
+  }
+
   useEffect(() => {
     scrollToTop();
-    setAdaptiveLyrics(generateLyrics(lyrics, fontSize));
+    generateWrappedLyrics();
     // let allChords = new Set<string>();
     // lyrics.forEach(({line, isChordsRow}) => {
     //   if (isChordsRow) {
@@ -40,7 +44,7 @@ const Song: FC<SongProps> = ({ author, song: { name, lyrics, speed: defaultSpeed
     // setChords(Array.from(allChords));
   }, [lyrics]);
 
-  useWindowResize(() => setAdaptiveLyrics(generateLyrics(lyrics, fontSize)));
+  useWindowResize(generateWrappedLyrics);
 
   const onAuthorClick = () => goToAuthor(author);
 
@@ -74,7 +78,7 @@ const Song: FC<SongProps> = ({ author, song: { name, lyrics, speed: defaultSpeed
                 {line}
               </p>
             ) : (
-              <p key={`row-${index}`} className={cn(styles.row, {[styles.chorus]: line.startsWith('Припев:')})}>
+              <p key={`row-${index}`} className={cn(styles.row, {[styles.chorus]: isChorusLine(line)})}>
                 {line}
               </p>
             );
@@ -89,71 +93,5 @@ const Song: FC<SongProps> = ({ author, song: { name, lyrics, speed: defaultSpeed
     </>
   );
 };
-
-function generateLyrics(lyrics: ISongRow[], fontSize: number){
-  const screenWidth = window.innerWidth - 32;
-  const newLyrics: ISongRow[] = [];
-
-  const calcWidth = (line: string) => getTextWidth(line, fontSize);
-
-  // console.log(lyrics);
-  for (let i = 0; i < lyrics.length; ++i) {
-    const { line, isChordsRow } = lyrics[i];
-    const lineWidth = calcWidth(line);
-    const trimmedLine = line.trim();
-
-    if (trimmedLine === '\n' || !trimmedLine.length) {
-      newLyrics.push({ line });
-      continue;
-    }
-
-    if (isChordsRow && i + 1 < lyrics.length && !lyrics[i + 1].isChordsRow) {
-      let chordsLine = line;
-      let songLine = lyrics[i + 1].line;
-      const songLineWidth = calcWidth(songLine);
-
-      if (songLineWidth > screenWidth) {
-        const delta = Math.round(songLine.length * screenWidth / songLineWidth);
-
-        while (songLine.length || chordsLine.length) {
-          const lastSpaceIndexInSongPart = songLine.lastIndexOf(' ', delta);
-
-          if (chordsLine.length) {
-            // TODO: проверить перенос посередине аккорда
-            const lastSpaceIndexInChordsPart = chordsLine.length > delta ? chordsLine.lastIndexOf(' ', songLine.length > 1 ? lastSpaceIndexInSongPart : delta) : -1;
-            const chordsLinePart = chordsLine.substring(0, (lastSpaceIndexInChordsPart === -1 ? chordsLine.length : lastSpaceIndexInChordsPart) + 1);
-            newLyrics.push({line: chordsLinePart, isChordsRow: true });
-            chordsLine = lastSpaceIndexInChordsPart !== -1 ? chordsLine.substring(lastSpaceIndexInChordsPart + 1, chordsLine.length) : '';
-          }
-
-          if (songLine.length) {
-            const songDelta = lastSpaceIndexInSongPart === -1 || !chordsLine.length ? songLine.length : lastSpaceIndexInSongPart + 1;
-            const songLinePart = songLine.substring(0, songDelta);
-            newLyrics.push({line: songLinePart });
-            songLine = songLine.length > songDelta ? songLine.substring(songDelta, songLine.length) : '';
-          }
-        }
-        i++;
-      } else if (lineWidth > screenWidth) {
-        const delta = Math.round(chordsLine.length * screenWidth / lineWidth);
-
-        while (chordsLine.length) {
-          const lastSpaceIndex = chordsLine.lastIndexOf(' ', delta);
-          const chordsLinePart = chordsLine.substring(0, (lastSpaceIndex === -1 ? chordsLine.length : lastSpaceIndex) + 1);
-          newLyrics.push({line: chordsLinePart, isChordsRow: true });
-          chordsLine = lastSpaceIndex !== -1 ? chordsLine.substring(lastSpaceIndex + 1, chordsLine.length) : '';
-        }
-      } else {
-        newLyrics.push({ line, isChordsRow });
-      }
-
-      // TODO: переработать и избавиться от continue
-      continue ;
-    }
-
-    newLyrics.push({ line, isChordsRow });
-  }
-  return newLyrics;
-}
 
 export default Song;
---
+++ src/containers/Song/helper.ts
@@ -0,0 +1,72 @@
+import { ISongRow } from '../../constants/SongsData';
+import { getTextWidth } from '../../utils/canvasHelper';
+
+export function isChorusLine(line: string) {
+  return line.match(/^(Припев|Chorus):/i);
+}
+
+export function generateLyrics(lyrics: ISongRow[], fontSize: number){
+  const screenWidth = window.innerWidth - 32;
+  const newLyrics: ISongRow[] = [];
+
+  const calcWidth = (line: string) => getTextWidth(line, fontSize);
+
+  // console.log(lyrics);
+  for (let i = 0; i < lyrics.length; ++i) {
+    const { line, isChordsRow } = lyrics[i];
+    const lineWidth = calcWidth(line);
+    const trimmedLine = line.trim();
+
+    if (trimmedLine === '\n' || !trimmedLine.length) {
+      newLyrics.push({ line });
+      continue;
+    }
+
+    if (isChordsRow && i + 1 < lyrics.length && !lyrics[i + 1].isChordsRow) {
+      let chordsLine = line;
+      let songLine = lyrics[i + 1].line;
+      const songLineWidth = calcWidth(songLine);
+
+      if (songLineWidth > screenWidth) {
+        const delta = Math.round(songLine.length * screenWidth / songLineWidth);
+
+        while (songLine.length || chordsLine.length) {
+          const lastSpaceIndexInSongPart = songLine.lastIndexOf(' ', delta);
+
+          if (chordsLine.length) {
+            // TODO: проверить перенос посередине аккорда
+            const lastSpaceIndexInChordsPart = chordsLine.length > delta ? chordsLine.lastIndexOf(' ', songLine.length > 1 ? lastSpaceIndexInSongPart : delta) : -1;
+            const chordsLinePart = chordsLine.substring(0, (lastSpaceIndexInChordsPart === -1 ? chordsLine.length : lastSpaceIndexInChordsPart) + 1);
+            newLyrics.push({line: chordsLinePart, isChordsRow: true });
+            chordsLine = lastSpaceIndexInChordsPart !== -1 ? chordsLine.substring(lastSpaceIndexInChordsPart + 1, chordsLine.length) : '';
+          }
+
+          if (songLine.length) {
+            const songDelta = lastSpaceIndexInSongPart === -1 || !chordsLine.length ? songLine.length : lastSpaceIndexInSongPart + 1;
+            const songLinePart = songLine.substring(0, songDelta);
+            newLyrics.push({line: songLinePart });
+            songLine = songLine.length > songDelta ? songLine.substring(songDelta, songLine.length) : '';
+          }
+        }
+        i++;
+      } else if (lineWidth > screenWidth) {
+        const delta = Math.round(chordsLine.length * screenWidth / lineWidth);
+
+        while (chordsLine.length) {
+          const lastSpaceIndex = chordsLine.lastIndexOf(' ', delta);
+          const chordsLinePart = chordsLine.substring(0, (lastSpaceIndex === -1 ? chordsLine.length : lastSpaceIndex) + 1);
+          newLyrics.push({line: chordsLinePart, isChordsRow: true });
+          chordsLine = lastSpaceIndex !== -1 ? chordsLine.substring(lastSpaceIndex + 1, chordsLine.length) : '';
+        }
+      } else {
+        newLyrics.push({ line, isChordsRow });
+      }
+
+      // TODO: переработать и избавиться от continue
+      continue ;
+    }
+
+    newLyrics.push({ line, isChordsRow });
+  }
+  return newLyrics;
+}
