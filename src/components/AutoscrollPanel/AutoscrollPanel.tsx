7cb19a1ba133c4ed6699afd2e25940adaf8587cb
ggwin-go <ggwin-go@yandex.ru>
2023-07-01 19:31:50 +0300

    auto scrolling refactored

--- src/components/AutoscrollPanel/AutoscrollPanel.tsx
+++ src/components/AutoscrollPanel/AutoscrollPanel.tsx
@@ -10,19 +10,46 @@ import scrollDownIcon from '../../assets/double-down.png';
 import styles from './AutoscrollPanel.module.scss';
 
 const DEFAULT_SPEED = 70;
-const TIMEOUT = 5;
+const TIMEOUT = 0;
 
 const AutoscrollPanel: FC = () => {
   const location = useLocation();
   const [time, setTime] = useState<number>(-1);
   const [scrollBtnLabel, setScrollBtnLabel] = useState<string>('!');
   const [scrollable, setScrollable] = useState<boolean>(false);
+  const [paused, setPaused] = useState<boolean>(false);
 
   const stopScroll = () => {
     setScrollable(false);
     setScrollBtnLabel('!');
     setTime(-1);
   };
+
+  const activatePauseScroll = () => setPaused(true);
+  const deactivatePauseScroll = () => setPaused(false);
+
+  const stopScrollAfterEnd = () => {
+    const pageDiv = document.getElementById('base');
+    if (pageDiv && window.scrollY + window.innerHeight >= pageDiv.scrollHeight) {
+      stopScroll();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', activatePauseScroll);
+    document.addEventListener('mouseup', deactivatePauseScroll);
+    document.addEventListener('touchstart', activatePauseScroll);
+    document.addEventListener('touchend', deactivatePauseScroll);
+    document.addEventListener('scroll', stopScrollAfterEnd);
+
+    return () => {
+      document.removeEventListener('mousedown', activatePauseScroll);
+      document.removeEventListener('mouseup', deactivatePauseScroll);
+      document.removeEventListener('touchstart', activatePauseScroll);
+      document.removeEventListener('touchend', deactivatePauseScroll);
+      document.removeEventListener('scroll', stopScrollAfterEnd);
+    };
+  }, []);
 
   useEffect(stopScroll, [location]);
 
@@ -40,7 +67,7 @@ const AutoscrollPanel: FC = () => {
     return () => window.clearTimeout(timeout);
   }, [time]);
 
-  useInterval(() => window.scrollBy({ top: 0.5, behavior: 'smooth' }), scrollable ? DEFAULT_SPEED : null);
+  useInterval(() => window.scrollBy({ top: 0.5, behavior: 'smooth' }), scrollable && !paused ? DEFAULT_SPEED : null);
 
   const toggleScrollable = async () => {
     if (time > 0 || scrollable) {
