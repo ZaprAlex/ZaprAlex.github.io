fe4392711b85542062e04ed1d88bae86f62ce998
ggwin-go <ggwin-go@yandex.ru>
2023-07-12 22:51:51 +0300

    close button added to settings panel

--- src/components/PopUp/SettingsPanelPopUp/SettingsPanelPopUp.tsx
+++ src/components/PopUp/SettingsPanelPopUp/SettingsPanelPopUp.tsx
@@ -1,17 +1,28 @@
 import React, { FC } from 'react';
 
+import { useModal } from '../../../hooks';
 import AutoscrollSwitch from './AutoscrollSwitch';
 import FullscreenSwitch from './FullscreenSwitch';
 import ThemeSwitch from './ThemeSwitch';
+import DoubleArrowRight from '../../../assets/DoubleArrowRight';
 
 import styles from './SettingsPanelPopUp.module.scss';
 
-const SettingsPanelPopUp: FC = () => (
-  <div className={styles.panel}>
-    <AutoscrollSwitch />
-    <ThemeSwitch />
-    <FullscreenSwitch />
-  </div>
-);
+const SettingsPanelPopUp: FC = () => {
+  const { closeModal } = useModal();
+
+  return (
+    <div className={styles.panel}>
+      <div className={styles.settings}>
+        <AutoscrollSwitch />
+        <ThemeSwitch />
+        <FullscreenSwitch />
+      </div>
+      <div onClick={closeModal} className={styles.cancelButton}>
+        <DoubleArrowRight />
+      </div>
+    </div>
+  );
+};
 
 export default SettingsPanelPopUp;
